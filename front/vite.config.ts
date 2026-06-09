2f085f50a35d31846bb579416e51390ddba83400
Matheus Malta <matheushtms04@gmail.com>
2026-06-09 14:51:37 -0300

    Mudar diretorio de output do front para build para consertar build no Vercel

--- front/vite.config.ts
+++ front/vite.config.ts
@@ -31,6 +31,10 @@ export default defineConfig({
     },
   },
 
+  build: {
+    outDir: 'build',
+  },
+
   // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
